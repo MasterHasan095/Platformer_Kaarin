8a4f3575409126ce6a914e673c4e56ad0386ac71
masterha <masterha@sheridancollege.ca>
2023-11-20 22:52:36 -0500

    EP 4 Completed, image added

--- src/main/java/com/example/platformer/inputs/KeyboardInputs.java
+++ src/main/java/com/example/platformer/inputs/KeyboardInputs.java
@@ -20,19 +20,18 @@ public class KeyboardInputs implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()){
-            case KeyEvent.VK_W:
+            case KeyEvent.VK_W :
                 gamePanel.changeYDelta(-5);
                 break;
-            case KeyEvent.VK_A:
-                gamePanel.changeXDelta(-5);
-                break;
-            case KeyEvent.VK_S:
+            case KeyEvent.VK_S :
                 gamePanel.changeYDelta(5);
                 break;
-            case KeyEvent.VK_D:
+            case KeyEvent.VK_A :
+                gamePanel.changeXDelta(-5);
+                break;
+            case KeyEvent.VK_D :
                 gamePanel.changeXDelta(5);
                 break;
-
         }
     }
 
--- src/main/java/com/example/platformer/main/GamePanel.java
+++ src/main/java/com/example/platformer/main/GamePanel.java
@@ -3,55 +3,60 @@ package com.example.platformer.main;
 import com.example.platformer.inputs.KeyboardInputs;
 import com.example.platformer.inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
-    private float xDelta = 100, yDelta = 100;
-    private float xDir =0.3f, yDir = 0.3f;
-    private int frames = 0;
-    private Long lastCheck = 0L;
-    public GamePanel(){
+
+    private int x = 100;
+    private int y = 100;
+    private BufferedImage img, subImg;
+
+    public GamePanel() {
         mouseInputs = new MouseInputs(this);
+
+        importing();
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
     }
 
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
+    public void importing() {
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
 
-        updateRectangle();
-
-        g.setColor(new Color(150, 20, 90  ));
-        g.fillRect((int)xDelta, (int)yDelta, 200, 50);
-
-    }
-
-    public void updateRectangle(){
-        xDelta += xDir;
-        if (xDelta > 400 || xDelta < 0){
-            xDir *= -1;
-        }
-        yDelta += yDir;
-        if (yDelta > 400 || yDelta < 00){
-            yDir *= -1;
+        try {
+            img = ImageIO.read(is);
+        }catch (IOException e){
+            e.printStackTrace();
         }
     }
 
     public void changeXDelta(int value){
-        this.xDelta += value;
+        this.x += value;
     }
     public void changeYDelta(int value){
-        this.yDelta += value;
+        this.y += value;
     }
 
-    public void setRectPos(int x , int y){
-        this.xDelta =x;
-        this.yDelta = y;
+    public void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
     }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        subImg = img.getSubimage(1*64, 8*40, 64, 40);
+        g.drawImage(subImg, x, y, 128, 80, null);
+    }
+
+
 }
